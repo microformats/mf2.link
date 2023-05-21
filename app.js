62372b5240fe10afee6b565aa0f604b889beeb2b
capjamesg <jamesg@jamesg.blog>
2023-05-21 11:30:27 +0100

    add better error handling, fix bugs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs = require("ejs");
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const PORT = process.env.PORT || 3010;
+var Microformats = require("microformat-node"), Cheerio = require('cheerio'), options = {};
 
 const app = express();
 
@@ -14,6 +15,69 @@ app.use(express.json());
 
 const SERVICES = {
     "mastodon": "mastodon"
+}
+
+
+function compileVCard (last_name, first_name, additional_name, honorific_prefix, honorific_suffix, full_name, organization, title, tel, email, url) {
+    var final_vcard = `
+    BEGIN:VCARD
+    VERSION:3.0
+    `;
+
+    if (first_name || last_name) {
+        first_name = first_name || "";
+        last_name = last_name || "";
+        additional_name = additional_name || "";
+        honorific_prefix = honorific_prefix || "";
+        honorific_suffix = honorific_suffix || "";
+        
+        final_vcard += `
+        N:${last_name};${first_name};${additional_name};${honorific_prefix};${honorific_suffix}
+        `;
+    }
+
+    final_vcard += `
+    FN:${full_name}
+    `;
+
+    final_vcard += `
+    ORG:${organization}
+    `;
+
+    final_vcard += `
+    TITLE:${title}
+    `;
+
+    if (tel) {
+        final_vcard += `
+        TEL;TYPE=home,voice;VALUE=uri:tel:${tel}
+        `;
+    }
+
+    // add email
+    final_vcard += `
+    EMAIL:${email}
+    `;
+
+    // add url
+    final_vcard += `
+    URL:${url}
+    `;
+
+    // end vcard
+    final_vcard += `
+    END:VCARD
+    `;
+
+    // strip whitespace
+    final_vcard = final_vcard.replace(/^\s+|\s+$/g, '');
+
+    return final_vcard;
+}
+
+function get(object, key, default_value) {
+    var result = object[key];
+    return (typeof result !== "undefined") ? result : default_value;
 }
 
 app.route("/").get(async (req, res) => {
@@ -75,10 +139,22 @@ app.route("/mastodon").get(async (req, res) => {
             "Content-Type": "application/json"
         }
     }).then((response) => {
+        // if not json, error
+        if (!response.ok) {
+            res.render("error", {
+                error: "There was an error retrieving this post."
+            });
+            return;
+        }
         response.json().then((data) => {
+            // if not json, error
+            if (!data) {
+                res.render("error", {
+                    error: "There was an error retrieving this post."
+                });
+                return;
+            }
             var is_reply = data.in_reply_to_id != null;
-
-            // replace publish date with pretty date
 
             var date = new Date(data.created_at);
 
@@ -118,11 +194,120 @@ app.route("/mastodon").get(async (req, res) => {
                     data: data,
                     reply: null
                 });
-            }
+            } 
+        }).catch((err) => {
+            res.render("error", {
+                error: "There was an error retrieving this post."
+            });
+            return;
         });
     }).catch((err) => {
         res.render("error", {
             error: "There was an error retrieving this post."
+        });
+        return;
+    });
+});
+
+app.route("/hcard").get(async (req, res) => {
+    var url = req.query.url;
+
+    if (!url) {
+        res.render("error", {
+            error: "Invalid URL"
+        });
+        return;
+    }
+
+    try {
+        var parsed_url = new URL(url);
+    } catch (e) {
+        res.render("error", {
+            error: "Invalid URL"
+        });
+        return;
+    }
+
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "text/html"
+        }
+    }).then((response) => {
+        response.text().then((data) => {
+            console.log(data);
+            options.html = data;
+
+            Microformats.get(options, function (err, data) {
+                // con
+                console.log(data, err)
+                if (err) {
+                    res.render("error", {
+                        error: "There was an error parsing this page."
+                    });
+                    return;
+                }
+
+                var hcard = null;
+
+                // find hcard
+                for (var i = 0; i < data.items.length; i++) {
+                    // console.log(data.items[i]);
+                    if (data.items[i].type.includes("h-card")) {
+                        hcard = data.items[i].properties;
+                        break;
+                    }
+                }
+
+                if (!hcard) {
+                    // console.log(hcard);
+                    res.render("error", {
+                        error: "No h-card was found."
+                    });
+                    return;
+                }
+
+                console.log(hcard)
+
+                // if org, expand out
+                if (hcard.org) {
+                    var org_titles = [];
+                    var orgs = [];
+
+                    for (var i = 0; i < hcard.org.length; i++) {
+                        var org = hcard.org[i].value;
+                        var title = hcard.org[i].properties.name;
+
+                        orgs.push(org);
+                        org_titles.push(title);
+                    }
+
+                    // join orgs as string
+                    hcard.title = org_titles.join(",").replace(/,/g, ", ");
+                    hcard.org = orgs.join(",").replace(/,/g, ", ");
+                }
+
+                var vcard = compileVCard(
+                    get(hcard, "family-name", [null])[0],
+                    get(hcard, "given-name", [null])[0],
+                    get(hcard, "additional-name", [null])[0],
+                    get(hcard, "honorific-prefix", [null])[0],
+                    get(hcard, "honorific-suffix", [null])[0],
+                    get(hcard, "name", [null])[0],
+                    get(hcard, "org", [null]),
+                    get(hcard, "title", [null]),
+                    get(hcard, "tel", [null])[0],
+                    get(hcard, "email", [null])[0],
+                    url
+                )
+
+                res.set("Content-Type", "text/plain");
+                res.send(vcard);
+            });
+        });
+    }).catch((err) => {
+        res.render("error", {
+            error: "There was an error retrieving this page."
         });
         return;
     });
